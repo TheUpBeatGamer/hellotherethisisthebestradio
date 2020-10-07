c73280b021390617bb64aeeb3346a1ca89ddb1c3
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 02:20:17 +0100

    Update Home.php

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -2,21 +2,21 @@
     <div class="row">
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
-                <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
+                <img src="https://owncloud.damonon.yt/index.php/apps/files_sharing/ajax/publicpreview.php?file=%2F%2Fdefault.png&c=8d0c8e9c3d70d0a16d401ad4ce89fd28&x=1920&y=1920&a=1&t=KdHTOy2XiIjB5CP">
                 <h1>now</h1>
                 <h2>Auto DJ</h2>
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
-                <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
+                <img src="https://owncloud.damonon.yt/index.php/apps/files_sharing/ajax/publicpreview.php?file=%2F%2Fdefault.png&c=8d0c8e9c3d70d0a16d401ad4ce89fd28&x=1920&y=1920&a=1&t=KdHTOy2XiIjB5CP">
                 <h1>next</h1>
                 <h2>Auto DJ</h2>
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
-                <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
+                <img src="https://owncloud.damonon.yt/index.php/apps/files_sharing/ajax/publicpreview.php?file=%2F%2Fdefault.png&c=8d0c8e9c3d70d0a16d401ad4ce89fd28&x=1920&y=1920&a=1&t=KdHTOy2XiIjB5CP">
                 <h1>later</h1>
                 <h2>Auto DJ</h2>
             </div>
