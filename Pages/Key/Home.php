be91331e029e81806a313c0b3ec612e25aa45255
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 04:02:59 +0100

    Update Home.php

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -90,7 +90,7 @@
                 </div>
             </div>
             <div style="padding-top: 25px;" class="row">
-            <div style="width: 93%; margin-left: 17px;" class="subline-title">Latest Media</div>
+            <div style="width: 93% !important; margin-left: 17px;" class="subline-title">Latest Media</div>
             <div style="margin-left: 17px;" class="subline"></div>
                 <div class="col-sm-12 col-md-6">
                     <div class="news-box">
