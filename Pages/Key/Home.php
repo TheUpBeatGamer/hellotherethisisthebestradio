63dd1f16d0cddecc6fbc5a4bf9fd309987992096
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 01:56:48 +0100

    Update Home.php

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -7,12 +7,12 @@
         </div>
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
-
+                <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
-
+                <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
             </div>
         </div>
     </div>
