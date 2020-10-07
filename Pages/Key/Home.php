a8c339433727e31cdda7e44948d3051f532c7fa7
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 01:47:18 +0100

    home

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -1,17 +1,17 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-4">
-            <div class="box">
+            <div class="nnl-box">
 
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
-            <div class="box">
+            <div class="nnl-box">
 
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
-            <div class="box">
+            <div class="nnl-box">
 
             </div>
         </div>
