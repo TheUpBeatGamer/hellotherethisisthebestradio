ba1612d74875eda2a51381b46d80f13f5181e7aa
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 02:18:32 +0100

    Update Home.php

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -10,14 +10,14 @@
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
                 <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
-                <h1>now</h1>
+                <h1>next</h1>
                 <h2>Auto DJ</h2>
             </div>
         </div>
         <div class="col-sm-12 col-md-4">
             <div class="nnlbox">
                 <img src="https://staff.keyfm.net/profilePictures/39-8224.png">
-                <h1>now</h1>
+                <h1>later</h1>
                 <h2>Auto DJ</h2>
             </div>
         </div>
