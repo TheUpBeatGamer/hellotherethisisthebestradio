2caf65ae519f59d0de652ad4d8ae9f51e0a090de
TheUpBeatGamer <theupbeatgamer@outlook.com>
2020-10-07 01:45:52 +0100

    Update Home.php

--- Pages/Key/Home.php
+++ Pages/Key/Home.php
@@ -1,13 +1,19 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-4">
-        One of three columns
+            <div class="box">
+
+            </div>
         </div>
         <div class="col-sm-12 col-md-4">
-        One of three columns
+            <div class="box">
+
+            </div>
         </div>
         <div class="col-sm-12 col-md-4">
-        One of three columns
+            <div class="box">
+
+            </div>
         </div>
     </div>
 </div>
